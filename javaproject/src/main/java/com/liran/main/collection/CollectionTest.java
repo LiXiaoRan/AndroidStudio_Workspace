a2e6708dde4d99d5cfe574c98805b0b97ecf08ad
liran <qq997843911@gmail.com>
2015-10-08 14:48:20 +0800

    开始练习关于反射的知识

--- javaproject/src/main/java/com/liran/main/collection/CollectionTest.java
+++ javaproject/src/main/java/com/liran/main/collection/CollectionTest.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by liran on 2015-10-05.
@@ -28,6 +31,17 @@ public class CollectionTest {
         list.set(1, 99);  //把1号位置的元素替换为99
 //        list.add(21);//直接通过Arrays.asList输出将其当作list，底层表示是数组 不能改变尺寸 因此add或者delete都会报错
         print2(list.iterator());
+
+        Set<Integer> intset = new TreeSet<>(list);
+        System.out.printf("\nsort:");
+        print2(intset.iterator());
+
+        Set<String> set1 = new HashSet<String>();
+        Collections.addAll(set1, "A B C D E F".split(" "));
+        System.out.printf("\n:");
+        print3(set1.iterator());
+        System.out.printf("\n:");
+        System.out.println(set1.toString());
     }
 
     public static void print(Collection<Integer> collection) {
@@ -40,7 +54,6 @@ public class CollectionTest {
 
     /**
      * 通过迭代器遍历   快捷键itit  迭代器最大的有点就是不用考虑容器是属于 set还是list还是map，都通用。
-     *
      * @param iterator
      */
     public static void print2(Iterator<Integer> iterator) {
@@ -50,4 +63,18 @@ public class CollectionTest {
             System.out.printf(" " + i + " ");
         }
     }
+
+    /**
+     * 通过迭代器遍历   快捷键itit  迭代器最大的有点就是不用考虑容器是属于 set还是list还是map，都通用。
+     *
+     * @param iterator
+     */
+    public static void print3(Iterator<String> iterator) {
+
+        while (iterator.hasNext()) {
+            String i = iterator.next();
+            System.out.printf(i);
+        }
+    }
+
 }
