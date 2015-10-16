4b2f07c763e22a59ce6d75d5594b17c5e00db0ca
liran <qq997843911@gmail.com>
2015-10-16 20:49:38 +0800

    练习了一下java流

---
+++ javaproject/src/main/java/com/liran/main/Stream/NodeStream.java
@@ -0,0 +1,33 @@
+package com.liran.main.Stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 测试节点流
+ * Created by liran on 2015-10-16.
+ * java的默认工作目录是 E:\androidstudio_workespace\MyApplication\
+ */
+public class NodeStream {
+
+    public static void main(String[] args) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("test.txt");
+             FileInputStream fileInputStream = new FileInputStream("E:\\androidstudio_workespace\\MyApplication\\javaproject\\src\\main\\java\\com\\liran\\main\\Stream\\FilewriterTest.java");
+        ) {
+
+            byte[] buff = new byte[32];
+            int hasread = 0;
+
+
+            while ((hasread = fileInputStream.read(buff)) > 0) {
+                System.out.printf(new String(buff, 0, hasread));
+                fileOutputStream.write(buff, 0, hasread);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ javaproject/src/main/java/com/liran/main/Stream/ProcessingStream.java
@@ -0,0 +1,25 @@
+package com.liran.main.Stream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * 测试处理流
+ * Created by liran on 2015-10-16.
+ */
+public class ProcessingStream {
+
+    public static void main(String[] args) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("ProcessingStreamTest.txt");
+             PrintStream printStream = new PrintStream(fileOutputStream);
+        ) {
+
+            printStream.println("---------------------- test ProcessingStream ------------------- ");
+            printStream.println(new ProcessingStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
