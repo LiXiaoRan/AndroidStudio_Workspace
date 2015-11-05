8902f7bbd8a4482ce0a4ae5c669540eca649c835
liran <qq997843911@gmail.com>
2015-11-05 19:32:39 +0800

    一个客户端和服务器通讯的小例子

---
+++ javaproject/src/main/java/com/liran/main/NetWork/SimpleClientSocket1.java
@@ -0,0 +1,86 @@
+package com.liran.main.NetWork;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 测试客户端socket的简单时例子
+ * Created by LiRan on 2015-11-05.
+ */
+public class SimpleClientSocket1 {
+
+    public static final int PORT = 8080;
+
+    private static Socket socket;
+
+    public static void main(String[] args) {
+
+        try {
+            InetAddress inetAddress = InetAddress.getByName(null);
+            System.out.println("addr is =" + inetAddress);
+            socket = new Socket(inetAddress, PORT);
+
+            System.out.println("客户端启动 socket is ：" + socket);
+
+
+            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//            PrintWriter printWriter=new PrintWriter(socket.getOutputStream());
+//            printWriter.println(ByteBuffer.wrap("我是客户端呀~~~\n".getBytes("UTF-8")));
+            printWriter.println("我是客户端呀~~~~~~");
+            System.out.println("sout我是客户端呀~~~");
+
+            String Str = null;
+            /*StringBuilder sb = new StringBuilder();
+            while ((Str = bufferedReader.readLine()) != null) {
+                sb.append(Str);
+                System.out.printf("str : "+Str);
+            }
+
+            System.out.println("服务器对客户端说：" + sb);*/
+
+
+           /* while (true) {
+                Str = bufferedReader.readLine();
+                if (Str.equals("END")) break;
+                System.out.println("服务器说： " + Str.toString());
+                printWriter.println("客户端说：" + Str.toString());
+
+            }*/
+
+            for (int i = 0; i < 10; i++) {
+                printWriter.println("你好呀~ " + i);
+                Str = bufferedReader.readLine();
+                System.out.println(Str.toString());
+            }
+
+
+            printWriter.println("END");
+
+        } catch (UnknownHostException e) {
+            System.out.println("异常：找不到主机");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null)
+                try {
+                    System.out.println("-------------客户端关闭-------------");
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+
+    }
+
+
+}
---
+++ javaproject/src/main/java/com/liran/main/NetWork/SimpleServerSocket1.java
@@ -0,0 +1,73 @@
+package com.liran.main.NetWork;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 测试一下服务器端的socket 简单的实例
+ * Created by LiRan on 2015-11-05.
+ */
+public class SimpleServerSocket1 {
+
+    public static final int PORT = 8080;
+
+    public static Socket clientSocket;
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = null;
+        try {
+            System.out.println("--------------服务器启动---------------");
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("服务器端的socket：" + serverSocket);
+
+
+            clientSocket = serverSocket.accept();
+            System.out.println("获取客户端的socket：" + clientSocket);
+            //服务器端用来读取客户端信息的缓冲流
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            //服务器端用来向客户端发送消息的缓冲流  true参数是自动刷新  println、printf 或 format 方法将刷新输出缓冲区
+            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+
+            /*printWriter.println("我是服务器呀~~~");
+            System.out.println("sout我是服务器呀~~~");*/
+
+            String clientStr = null;
+
+
+            /**这是一个教训，这里前往不能写readline作为判断条件，因为他们没有判断换行
+             StringBuilder sb = new StringBuilder();
+             while ((clientStr = bufferedReader.readLine()) != null) {
+             sb.append(clientStr);
+             System.out.printf("clientStr: "+clientStr);
+             }*/
+
+            while (true) {
+                clientStr = bufferedReader.readLine();
+                if (clientStr.equals("END")) break;
+                System.out.println("客户端说： " + clientStr.toString());
+                printWriter.println("服务器说：" + clientStr.toString());
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("--------------服务器关闭---------------");
+            if (serverSocket != null)
+                serverSocket.close();
+            if (clientSocket != null)
+                clientSocket.close();
+        }
+
+
+    }
+
+}
