25cc5f1db65296b827408c4f321f45913bd22f0c
liran <qq997843911@gmail.com>
2015-11-29 19:56:26 +0800

    完善了一下

--- RemoteViews/src/main/java/layout/MyAppWidget.java
+++ RemoteViews/src/main/java/layout/MyAppWidget.java
@@ -1,11 +1,14 @@
 package layout;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.liran.remoteviews.R;
 
@@ -14,11 +17,13 @@ import com.liran.remoteviews.R;
  */
 public class MyAppWidget extends AppWidgetProvider {
 
-    private static final String TAG="MyAppWidget";
+    private static final String TAG = "MyAppWidget";
 
-    public static final String CLICK_ACTION="com.liran.remoteviews.action.CLICK";
+    public static final String CLICK_ACTION = "com.liran.remoteviews.action.CLICK";
+
     /**
      * 桌面小部件更新
+     *
      * @param context
      * @param appWidgetManager
      * @param appWidgetId
@@ -31,12 +36,17 @@ public class MyAppWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
+        Intent intentclick=new Intent();
+        intentclick.setAction(CLICK_ACTION);
+        PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intentclick,0);
+        views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     /**
      * 每次桌面小部件更新时都会回调一次该方法
+     *
      * @param context
      * @param appWidgetManager
      * @param appWidgetIds
@@ -62,18 +72,32 @@ public class MyAppWidget extends AppWidgetProvider {
 
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, final Intent intent) {
         super.onReceive(context, intent);
-        Log.d(TAG, "onReceive: action = "+intent.getAction());
+        Log.d(TAG, "onReceive: action = " + intent.getAction());
 
-        //这里是判断自己的action
-        if(intent.getAction().equals(CLICK_ACTION)){
+        if (intent.getAction().equals(CLICK_ACTION)) {
+            Toast.makeText(context, "点击了", Toast.LENGTH_SHORT).show();
 
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    AppWidgetManager appWidgetManager=AppWidgetManager.getInstance(context);
+                    RemoteViews remoteViews=new RemoteViews(context.getPackageName(),R.layout.my_app_widget);
+                    remoteViews.setTextViewText(R.id.appwidget_text, "点击时间：" + System.currentTimeMillis());
+//                    Intent intentClick=new Intent(context, MainActivity.class);
+                    Intent intentClick=new Intent();
+                    intentClick.setAction(CLICK_ACTION);
+                    PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intentClick,0);
+                    remoteViews.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
+//                    appWidgetManager.updateAppWidget(R.layout.my_app_widget,remoteViews);
+                    appWidgetManager.updateAppWidget(new ComponentName(context, MyAppWidget.class), remoteViews);
 
+                }
+            }).start();
 
         }
-
 
 
     }
