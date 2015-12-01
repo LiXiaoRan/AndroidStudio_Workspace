9b1a3f9ba2dec0056311d2b3c1c5d4fb18b114d0
liran <qq997843911@gmail.com>
2015-12-01 21:23:26 +0800

    属性动画 完善了一下

--- RemoteViews/src/main/java/com/liran/remoteviews/MainActivity.java
+++ RemoteViews/src/main/java/com/liran/remoteviews/MainActivity.java
@@ -20,15 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView= (TextView) findViewById(R.id.textview1);
-        button= (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textview1);
+        button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //这里是普通的view动画
                 Toast.makeText(MainActivity.this, "aaaaa", Toast.LENGTH_SHORT).show();
-                Animation animation= AnimationUtils.loadAnimation(MainActivity.this, R.anim.anmi_list);
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anmi_list);
                 button.startAnimation(animation);
 
                 startTextAnimator();
@@ -38,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
+    //属性动画
     private void startTextAnimator() {
-            //属性动画
+
         /**
          * 使用属性动画的时候，所操作的属性必须提供了get和set方法，如果这个属性拥有初始值，则不需要get方法，
          * 不然会闪退，然后就是需要对属性的修改通过某种变化反映出来(例如UI的变化)，不然没效果
@@ -51,13 +50,15 @@ public class MainActivity extends AppCompatActivity {
          * 2、用一个类来包装原始对象，间接提供get、set方法
          * 3、 采用valueAnimator,监听动画过程，自己实现属性的改变。
          * */
-        ViewWrapper viewWrapper=new ViewWrapper(textView);
-        ObjectAnimator.ofInt(viewWrapper,"width",500).setDuration(5000).start();
+        ViewWrapper viewWrapper = new ViewWrapper(textView);//这里是采用了第二种
+        ObjectAnimator.ofInt(viewWrapper, "width", 800).setDuration(5000).start();
     }
+
+
 }
 
 
-class ViewWrapper{
+class ViewWrapper {
 
     private View target;
 
@@ -65,12 +66,12 @@ class ViewWrapper{
         this.target = target;
     }
 
-    public int getWidth(){
+    public int getWidth() {
         return target.getLayoutParams().width;
     }
 
-    public void  setWidth(int width){
-        target.getLayoutParams().width=width;
+    public void setWidth(int width) {
+        target.getLayoutParams().width = width;
         target.requestLayout();
     }
 }
