aef9759c92d8097bc59870c4211617447a0e869d
liran <qq997843911@gmail.com>
2015-12-29 17:17:08 +0800

    一会晚上继续写，明天又要去练车

---
+++ CustomView/src/main/java/com/liran/custom_view/MyView/MatriView.java
@@ -0,0 +1,72 @@
+package com.liran.custom_view.MyView;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.liran.custom_view.R;
+import com.liran.custom_view.Utils.MeasureUtil;
+
+/**
+ * Created by LiRan on 2015-12-29.
+ */
+public class MatriView extends View {
+    private static final int RECT_SIZE = 400;// 矩形尺寸的一半
+
+    private Paint mPaint;// 画笔
+
+    private int left, top, right, bottom;// 矩形坐上右下坐标
+    private int screenX, screenY;
+
+    public MatriView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // 获取屏幕尺寸数据
+        int[] screenSize = MeasureUtil.getScreenWidth(context);
+
+        // 获取屏幕中点坐标
+        screenX = screenSize[0] / 2;
+        screenY = screenSize[1] / 2;
+
+        // 计算矩形左上右下坐标值
+        left = screenX - RECT_SIZE;
+        top = screenY - RECT_SIZE;
+        right = screenX + RECT_SIZE;
+        bottom = screenY + RECT_SIZE;
+
+        // 实例化画笔
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        // 获取位图
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bg);
+
+        // 实例化一个Shader
+        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+
+        // 实例一个矩阵对象
+        Matrix matrix = new Matrix();
+
+        // 设置矩阵变换
+        matrix.setTranslate(500, 500);
+
+        // 设置Shader的变换矩阵
+        bitmapShader.setLocalMatrix(matrix);
+
+        // 设置着色器
+        mPaint.setShader(bitmapShader);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // 绘制矩形
+        // canvas.drawRect(left, top, right, bottom, mPaint);
+        canvas.drawRect(0, 0, screenX * 2, screenY * 2, mPaint);
+    }
+}
---
+++ coutomviewlearn/src/main/java/com/liran/lenovo/coutomviewlearn/Utils/MeasureUtil.java
@@ -0,0 +1,55 @@
+package com.liran.lenovo.coutomviewlearn.Utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import java.lang.reflect.Field;
+
+/**
+ * 非常实用的工具类
+ * Created by lr on 2015-09-01.
+ */
+public class MeasureUtil {
+
+
+    /**
+     *获取屏幕尺寸的像素  包括标题栏和状态栏
+     * @param context
+     * @return int size[2]  size[0] 宽  size[1] 高
+     */
+    public static int [] getScreenWidth(Context context){
+        int size[]=new int[2];
+        DisplayMetrics displayMetrics=new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(displayMetrics);
+        size[0]=displayMetrics.widthPixels;
+        size[1]=displayMetrics.heightPixels;
+
+        return size;
+    }
+
+
+    /**
+     * 获取状态栏高度
+     * @param context
+     * @return statusBarHeight int
+     */
+    public static int getStatusBarHeight(Context context){
+        Class<?> c = null;
+        Object obj = null;
+        Field field = null;
+        int x = 0, statusBarHeight = 0;
+        try {
+            c = Class.forName("com.android.internal.R$dimen");
+            obj = c.newInstance();
+            field = c.getField("status_bar_height");
+            x = Integer.parseInt(field.get(obj).toString());
+            statusBarHeight = context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return statusBarHeight;
+    }
+
+}
