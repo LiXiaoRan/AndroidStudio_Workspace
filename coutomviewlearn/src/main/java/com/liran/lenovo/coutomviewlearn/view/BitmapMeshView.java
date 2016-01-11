9070cbbc7d0fb03887eb2ed5eac685637a1391f9
liran <qq997843911@gmail.com>
2016-01-11 13:26:03 +0800

    正准备写一写drawBitmapMesh

--- coutomviewlearn/src/main/java/com/liran/lenovo/coutomviewlearn/view/BitmapMeshView.java
+++ coutomviewlearn/src/main/java/com/liran/lenovo/coutomviewlearn/view/BitmapMeshView.java
@@ -1,29 +1,81 @@
 package com.liran.lenovo.coutomviewlearn.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.liran.lenovo.coutomviewlearn.R;
 
 /**
  * Created by LiRan on 2016-01-10.
  */
 public class BitmapMeshView extends View {
 
-    public static final int WIDTH=19; //横向分割成的网格数量
-    public static final int HEIGHT=19; //纵向分割成的网格数量
-    public static final int COUNT=(WIDTH+1)*(HEIGHT+1);
+    private static final int WIDTH = 19; //横向分割成的网格数量
+    private static final int HEIGHT = 19; //纵向分割成的网格数量
+    private static final int COUNT = (WIDTH + 1) * (HEIGHT + 1);
 
+    private Bitmap mBitmap;//位图资源
+
+    private float[] verts;//交点的坐标数组
 
     public BitmapMeshView(Context context) {
         super(context);
+        init();
     }
 
     public BitmapMeshView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public BitmapMeshView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
+    /**
+     * 初始化
+     */
+    private void init() {
+
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bg);
+
+
+        verts = new float[COUNT * 2];
+
+
+        /**
+         * 生成各个交点的坐标
+         */
+
+        int index = 0;
+        float multiple=mBitmap.getWidth();
+        for(int y=0;y<HEIGHT;y++){
+                float fy=mBitmap.getHeight()*y/HEIGHT;
+            for (int x = 0; x <= WIDTH; x++) {
+                float fx = mBitmap.getWidth() * x / WIDTH + ((HEIGHT - y) * 1.0F / HEIGHT * multiple);
+                setXY(fx, fy, index);
+                index += 1;
+            }
+
+    }
+
+}
+
+    private void setXY(float fx, float fy, int index) {
+        verts[index * 2 + 0] = fx;
+        verts[index * 2 + 1] = fy;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 绘制网格位图
+        canvas.drawBitmapMesh(mBitmap, WIDTH, HEIGHT, verts, 0, null, 0, null);
+    }
 }
