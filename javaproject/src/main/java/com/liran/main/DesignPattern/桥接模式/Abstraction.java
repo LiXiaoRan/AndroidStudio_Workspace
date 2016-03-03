1c53faf772c7888c3511cf27e43a3b64e0c992ef
liran <qq997843911@gmail.com>
2016-03-03 14:30:28 +0800

    写了桥接模式的示例代码。

---
+++ javaproject/src/main/java/com/liran/main/DesignPattern/桥接模式/Abstraction.java
@@ -0,0 +1,18 @@
+package com.liran.main.DesignPattern.桥接模式;
+
+/**
+ * 抽象
+ * Created by LiRan on 2016-03-03.
+ */
+public class Abstraction {
+
+    protected Implementor implementor;
+
+    public void setImplementor(Implementor implementor) {
+        this.implementor = implementor;
+    }
+
+    public void Operation(){
+        implementor.Operation();
+    }
+}
---
+++ javaproject/src/main/java/com/liran/main/DesignPattern/桥接模式/Implementor.java
@@ -0,0 +1,10 @@
+package com.liran.main.DesignPattern.桥接模式;
+
+/**
+ * 实现
+ * Created by LiRan on 2016-03-03.
+ */
+abstract public class Implementor {
+
+    public abstract void Operation();
+}
