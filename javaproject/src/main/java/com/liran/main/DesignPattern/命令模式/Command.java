47ed03ddb369a2286a9b1182db2c64d95a119b1f
liran <qq997843911@gmail.com>
2016-03-03 17:16:08 +0800

    写了命令模式的示例代码。

---
+++ javaproject/src/main/java/com/liran/main/DesignPattern/命令模式/Command.java
@@ -0,0 +1,11 @@
+package com.liran.main.DesignPattern.命令模式;
+
+/**
+ * 用来声明执行操作的接口
+ * Created by LiRan on 2016-03-03.
+ */
+public interface Command {
+
+    public void Execute();
+
+}
---
+++ javaproject/src/main/java/com/liran/main/DesignPattern/命令模式/Receiver.java
@@ -0,0 +1,16 @@
+package com.liran.main.DesignPattern.命令模式;
+
+/**
+ * 接收者
+ * Created by LiRan on 2016-03-03.
+ */
+public class Receiver {
+
+    public void Action(){
+
+        System.out.println("执行命令。");
+
+    }
+
+
+}
