d0d380d35f6fbf022e0ff9f1f8599044bfd4178d
liran <qq997843911@gmail.com>
2016-03-05 16:42:00 +0800

    完成了写享元模式的实例代码

--- javaproject/src/main/java/com/liran/main/DesignPattern/享元模式/Flyweight_pattern.java
+++ javaproject/src/main/java/com/liran/main/DesignPattern/享元模式/Flyweight_pattern.java
@@ -1,5 +1,7 @@
 package com.liran.main.DesignPattern.享元模式;
 
+import java.util.HashMap;
+
 /**
  * 享元模式
  * Created by LiRan on 2016-03-04.
@@ -9,6 +11,20 @@ public class Flyweight_pattern {
     public static void main(String[] args) {
 
 
+        FlyweightFactory f = new FlyweightFactory();
+
+        Flyweight flyweight1 = f.getFlyweight("X");
+        flyweight1.Operation(1);
+
+        Flyweight flyweight2 = f.getFlyweight("y");
+        flyweight2.Operation(2);
+
+
+        Flyweight flyweight3 = f.getFlyweight("z");
+        flyweight3.Operation(3);
+
+        UnsharedConcreteFlyweight uf = new UnsharedConcreteFlyweight();
+        uf.Operation(44);
 
     }
 
@@ -50,7 +66,28 @@ class UnsharedConcreteFlyweight extends Flyweight {
     }
 }
 
+/**
+ * 一个享元工厂，用来创建并管理Flyweight对象。它主要用
+ * 来确保合理的共享flyweight，当用户请求一个Flyweight
+ * 时，FlyweightFactory对象提供一个已创建的实例或者创建
+ * 一个（如果不存在的话）。
+ */
+
 class FlyweightFactory {
 
+    private HashMap<String, Flyweight> flyweights = new HashMap<>();
+
+
+    public FlyweightFactory() {
+        flyweights.put("X", new ConcreteFlyweight());
+        flyweights.put("y", new ConcreteFlyweight());
+        flyweights.put("z", new ConcreteFlyweight());
+
+    }
+
+    public Flyweight getFlyweight(String key) {
+
+        return flyweights.get(key);
+    }
 
 }
